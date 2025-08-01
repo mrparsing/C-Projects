feed8fe59428795e26904b3a0755074645e4bd99
mrparsing <fralindi5@gmail.com>
2025-08-01 18:52:26 +0200

    tictactoe: check_win function

--- project/tictactoe/tictactoe.c
+++ project/tictactoe/tictactoe.c
@@ -15,12 +15,79 @@ void print(char grid[3][3])
     printf("-------------\n");
 }
 
+int check_row(char grid[3][3], char player, int row)
+{
+    for (int col = 0; col < 3; col++)
+    {
+        if (grid[row][col] != player)
+            return 0;
+    }
+    return 1;
+}
+
+int check_col(char grid[3][3], char player, int col)
+{
+    for (int row = 0; row < 3; row++)
+    {
+        if (grid[row][col] != player)
+            return 0;
+    }
+    return 1;
+}
+
+int check_diagonal(char grid[3][3], char player)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (i == j && grid[i][j] != player)
+                return 0;
+        }
+    }
+    return 1;
+}
+
+int check_anti_diagonal(char grid[3][3], char player)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if ((2 - i) == j && grid[i][j] != player)
+                return 0;
+        }
+    }
+    return 1;
+}
+
+int check_win(char grid[3][3], char player)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if (check_row(grid, player, i))
+            return 1;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        if (check_col(grid, player, i))
+            return 1;
+    }
+    if (check_diagonal(grid, player))
+        return 1;
+    if (check_anti_diagonal(grid, player))
+        return 1;
+    return 0;
+}
+
 int main()
 {
     char grid[3][3] = {
-    {'O', 'O', 'O'},
-    {'O', 'O', 'O'},
-    {'O', 'O', 'O'}
-};
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}};
+    printf("Welcome to tictactoe game!\nYou are X, computer is O\n");
+    printf("Choose a position from 1 to 9\n");
     print(grid);
+    printf("%d\n", check_win(grid, 'X'));
 }
